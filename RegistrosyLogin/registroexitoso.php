b5689fa56d6316caded492f9a78dd25d9765c996
apazalucas-1998 <68450787+apazalucas-1998@users.noreply.github.com>
2020-10-27 20:24:12 -0300

    Update registroexitoso.php

--- RegistrosyLogin/registroexitoso.php
+++ RegistrosyLogin/registroexitoso.php
@@ -1,18 +1,3 @@
-<?php
-$servidor1 = "localhost";
-$usuario1 = "root";
-$clave1 = "";
-$basededatos1 = "formulariosregistro";
-
-$enlace1 = mysqli_connect($servidor1 , $usuario1 , $clave1 , $basededatos1);
-
-if(!$enlace1){
-    echo '<font color="red"><b>*error en el servidor</b></font>';
-}
-?>
-
-
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -20,6 +5,7 @@ if(!$enlace1){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TecnoLibre - Register</title>
+         <link rel="shortcut icon" href="../img/icon.png" type="image/x-icon">
     <link rel="stylesheet" href="../css3/registroexitoso.css">
     <link rel="stylesheet" href="../css/all.min.css">
     <script src="../js/jquery.js"></script>
@@ -38,8 +24,7 @@ if(!$enlace1){
                 <li><a href="../laptops.php">Laptops</a></li>  
                 <li><a href="../historia.php">Historias</a></li>
                 <li><a href="../descarga.php">Descargas</a></li>   
-              
-                     
+
                 <li style="position: relative; left: 65%;"><a href="../ayuda.php" title="Ayuda"><i style="font-size:25px;" class="fas fa-question-circle"></i></a></li>
                 <li style="position: relative; left: 65%;"><a href="../login.php" title="Ingresar"><i style="font-size:25px;" class="fas fa-sign-out-alt"></i></a></li>
             </ul>
@@ -60,37 +45,28 @@ if(!$enlace1){
                     </tr>
                 </thead>
                 <tbody>
-                    <?php
-                    $consulta1 = "SELECT * FROM registros";
-                    $ejecutarconsulta1 = mysqli_query($enlace1,$consulta1);
-                    $verfilas1 = mysqli_num_rows($ejecutarconsulta1);
-                    $fila1 = mysqli_fetch_array($ejecutarconsulta1);
-                    if(!$ejecutarconsulta1){
-                        echo '<font color ="red"><b>Error en la consultas</b></font>';
-                    }
-                    else {
-                        if($verfilas1 < 1){
-                            echo '<tr>
-                                      <td colspan="4" style="text-align:center;">Sin registros</td>
-                                  </tr>';
-                        } else{
-                            for($i = 0; $i <= $fila1; $i++){
-                                echo ' <tr>
-                                            <td  style="text-align:center; padding:10px;">'.$fila1[0].'</td>
-                                            <td  style="text-align:center;">'.$fila1[1].'</td>
-                                            <td  style="text-align:center;">'.$fila1[2].'</td>
-                                            <td  style="text-align:center;">'.$fila1[3].'</td>
-                                            <td  style="text-align:center;">'.$fila1[4].'</td>
-                                            
-                                        </tr>  
+             <?php
+             if(isset($_POST['registrar'])){
+               $id = $_POST["id"];
+               $nombre = $_POST["nombre"];
+               $apellido = $_POST["apellido"];
+               $correo = $_POST["correo"];
+               $user = $_POST["usuario"];
+               
+           
+             echo ' <tr>
+                       <td  style="text-align:center; padding:10px;">'.$id.'</td>
+                       <td  style="text-align:center;">'.$nombre.'</td>
+                       <td  style="text-align:center;">'.$apellido.'</td>
+                       <td  style="text-align:center;">'.$correo.'</td>
+                       <td  style="text-align:center;">'.$user.'</td>         
+                   </tr>  
+
                                         
-                                        ';
-
-                              $fila1 = mysqli_fetch_array($ejecutarconsulta1);
-                            }
-                        }
-
-                    }
+                ';
+            }
+                              
+                        
 
                  
 
@@ -125,35 +101,17 @@ if(!$enlace1){
 
 <?php
 
-if(isset($_POST['registrar'])){
-    $id = $_POST["id"];
-    $nombre1 = $_POST["nombre"];
-    $apellido = $_POST["apellido"];
-    $correo = $_POST["correo"];
-    $user = $_POST["usuario"];
 
-    $insert = "INSERT INTO registros VALUES('$id',
-                                           '$nombre1',
-                                           '$apellido',
-                                           '$correo',
-                                           '$user')";
+    
 
-    $ejecutar = mysqli_query($enlace1 , $insert);
-
-    if(!$ejecutar){
-        echo '<font color="red"><b>Error</b></font>';
-    }
-
-}
 
 ?>
 
 <?php
 
 
-
 $usuario = fopen("../csv/user.csv", "a+");
-$text = $id."  ".$nombre1."  ".$apellido." ".$correo." ".$user.chr(13).chr(10);
+$text = $id."  ".$nombre."  ".$apellido."  ".$correo."  ".$user.chr(13).chr(10);
 
 fwrite($usuario , $text);
 
@@ -161,32 +119,20 @@ fclose($usuario);
 
 
 
+$usuario = fopen("../csv/user.doc", "a+");
+$text = $id."  ".$nombre."  ".$apellido."  ".$correo."  ".$user.chr(13).chr(10);
 
-$id2 = $_POST["id"];
-$nombre2 = $_POST["nombre"];
-$apellido2 = $_POST["apellido"];
-$correo2 = $_POST["correo"];
-$user2 = $_POST["usuario"];
+fwrite($usuario , $text);
 
-$usuario2 = fopen("../csv/user.doc", "a+");
-$text2 =$id2."  ". $nombre2."   ".$apellido2." ".$correo2." ".$user2.chr(13).chr(10);
+fclose($usuario);
 
-fwrite($usuario2 , $text2);
 
-fclose($usuario2);
+$usuario = fopen("../csv/user.txt", "a+");
+$text = $id."  ".$nombre."  ".$apellido."  ".$correo."  ".$user.chr(13).chr(10);
 
-$id3 = $_POST["id"];
-$nombre3 = $_POST["nombre"];
-$apellido3 = $_POST["apellido"];
-$correo3 = $_POST["correo"];
-$user3 = $_POST["usuario"];
+fwrite($usuario , $text);
 
-$usuario3 = fopen("../csv/user.txt", "a+");
-$text3 =$id3."  ". $nombre3."   ".$apellido3." ".$correo3." ".$user3.chr(13).chr(10);
-
-fwrite($usuario3 , $text3);
-
-fclose($usuario3);
+fclose($usuario);
 
 ?>
 
@@ -221,6 +167,9 @@ fclose($usuario3);
 
 
 -->
+
+
+
 
 
 
